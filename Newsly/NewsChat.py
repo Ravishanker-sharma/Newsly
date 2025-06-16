1b7b0151d6c43d3a4a19e93ffea161fcf1eba86b
Ravishanker sharma <ravisharma87000@gmail.com>
2025-06-16 20:04:26 +0530

    Update NewsChat.py

--- Newsly/NewsChat.py
+++ Newsly/NewsChat.py
@@ -82,7 +82,7 @@ def run_chat_ui(headline):
 
                 llm = ChatGoogleGenerativeAI(
                     model="gemini-2.0-flash",
-                    api_key="AIzaSyA-Dk7dhyut4IZHFLBTSzQcC_tAaYwYqoU",
+                    api_key=[Your API],
                     temperature=0.5
                 )
 
