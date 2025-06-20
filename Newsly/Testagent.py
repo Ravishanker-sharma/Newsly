7323c1306e06c7ae4371b34dc5608802e85085e9
Ravishanker sharma <ravisharma87000@gmail.com>
2025-06-20 14:56:27 +0530

    Update Testagent.py

--- Newsly/Testagent.py
+++ Newsly/Testagent.py
@@ -10,7 +10,7 @@ import re
 # Google Gemini LLM setup
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
-    api_key=[Your API],
+    api_key="yourapi",
     temperature=0.5
 )
 
@@ -27,7 +27,10 @@ def format(response:List[Dict[str,List[str]]]):
 
 def extract_json_from_llm_output(text):
     # Remove Markdown code fences (```json or ```), if present
-    cleaned = re.sub(r"```json|```", "", text).strip()
+    if "json" in text:
+        cleaned = re.sub(r"```json|```", "", text).strip()
+    else:
+        cleaned = re.sub(r"```python|```", "", text).strip()
     try:
         data = json.loads(cleaned)
         return data
@@ -50,6 +53,18 @@ Format the response as a list of dictionaries like:
     ~ Do not return it like a Json , Handle it like a string
 """
 
+instructions2 = '''You will get the news and the data of user on basis of these two you have to tell how the news will affect the user.
+Write News in Hinglish language.
+Keep the sequence of news same.
+Summarize the affect on the user into 4 to 5 main bullet points.
+You should look like you are telling the news directly to the user.
+Avoid including irrelevant things in the Output.
+Your headline will always remain same : ~Impact On You~ , bullets will change.
+Format the response as a list of dictionaries like:
+    [{"title":"Headline","Bullets":["point1","point2","point3"]},{"title":"Headline","Bullets":["point1","point2","point3"]}]
+    ~ Do not return it like a Json , Handle it like a string
+'''
+
 llmwithtool = llm.bind_tools(tools)
 
 # Agent initialization
@@ -58,6 +73,14 @@ agent = initialize_agent(
     tools=tools,
     agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
     agent_kwargs={"system_message": instructions},
+    verbose=True
+)
+
+agent_for_custom = initialize_agent(
+    llm=llm,
+    tools=[],
+    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+    agent_kwargs={"system_message": instructions2},
     verbose=True
 )
 r=0
@@ -87,4 +110,16 @@ def get_ai_news():
     full_prompt = f"{instructions}\n{content}"
     result = llmwithtool.invoke(full_prompt)
     return extract_json_from_llm_output(result.content)
+
+def get_user_profile(url):
+    profile =""" Provide Profile Data
+"""
+    return profile
+
+def customized_news(url,news):
+    profile = get_user_profile(url)
+    prompt = f"Instructions : {instructions2} , User data : {profile}, News:{news}"
+    result2 = llm.invoke(prompt)
+    # return result2.content
+    return extract_json_from_llm_output(result2.content)
 
